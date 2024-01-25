7b7b586927ab083c64816951455793a23dd0afa0
atzina <atzina@gmail.com>
2024-01-24 19:04:02 -0600

    Primera propuesta de seccion Trabajo grilla

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,24 +1,33 @@
-import Compu from '../assets/compu.jpg';
+import './Work.css';
+import Compu from "../assets/compu.jpg";
+import Cima from "../assets/CIMA.png";
+import Diario from "../assets/diario-viaje.png";
+import Disc from "../assets/discapacidad.png";
+import Star from "../assets/star-wars.png";
+import Power from "../assets/power-bi.png";
+import Md from "../assets/mdlinks.png";
 
-function Work () {
-    return(
-        <>
-             <div className="aboutme">
-             
-              <div className="div-dos-aboutme">
-                <img className="image-aboutme" src={Compu}></img>
-              </div>
 
-              <div className="div-uno-aboutme">
-                <h1>Work</h1>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, illum aperiam blanditiis et quae assumenda! Magni similique doloribus rem necessitatibus doloremque. Quo accusantium modi optio id. Quis consectetur similique quod.
-                </p>
-               
-              </div>
+function Work() {
+  return (
+    <>
+      <div className="work">
+        <div className='title-work'><h1>Mi Trabajo</h1></div>
+        <div className="projects">
+          <div class="container text-center">
+            <div class="row">
+              <div class="col-sm-1 col-md-4 col-lg-4"><img className="compu" src={Cima}></img></div>
+              <div class="col-sm-1 col-md-4 col-lg-4"><img className="compu" src={Diario}></img></div>
+              <div class="col-sm-1 col-md-4 col-lg-4"><img className="compu" src={Disc}></img></div>
+              <div class="col-sm-1 col-md-4 col-lg-4"><img className="compu" src={Power}></img></div>
+              <div class="col-sm-1 col-md-4 col-lg-4"><img className="compu" src={Star}></img></div>
+              <div class="col-sm-1 col-md-4 col-lg-4"><img className="compu" src={Md}></img></div>
             </div>
-        </>
-    )
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Work;
