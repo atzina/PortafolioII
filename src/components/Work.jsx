00949f85aebeebd492ae23288a90bbfeec086b16
atzina <atzina@gmail.com>
2024-04-10 21:43:04 -0600

    botones para filtrar proyectos

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,22 +6,63 @@ import Disc from "../assets/discapacidad.png";
 import Star from "../assets/star-wars.png";
 import Power from "../assets/power-bi.png";
 import Md from "../assets/mdlinks.png";
+import React, { useState } from 'react';
 
 
 function Work() {
+  // define el estado pora la categoría seleccionada
+  const[category, setCategory] = useState('all'); // Inicialmente, muestra todas las imágenes
+
+  // define la función para manejar el cambio de categoría
+
+  const hadleCategoryChange = (newCategory) => {
+    setCategory(newCategory);
+  };
+
+  //array de objetos que contiene la inforamción de cada imagen y su categoría
+
+  const projectsData = [
+    {src:Cima, category: 'wordpress'},
+    {src:Diario, category: 'react'},
+    {src: Disc, category: 'react'},
+    {src: Power, category: 'power'},
+    {src: Star, category: 'react'},
+    {src: Md, category: 'nodejs'},
+  ];
+
+  //Filtra las imágenes según la categoría seleccionada
+
+  const filteredProjects = category === 'all' ? projectsData : projectsData.filter(project => project.category === category);
+
   return (
     <>
       <div className="work">
         <div className='title-work'><h1>Mi Trabajo</h1></div>
+        <div className='filter-buttons flex'>
+          <button onClick={() => hadleCategoryChange('all')}>Mostrar Todo</button>
+          <button onClick={() => hadleCategoryChange('wordpress')}>WordPress</button>
+          <button onClick={()=> hadleCategoryChange('react')}>React</button>
+          <button onClick={()=> hadleCategoryChange('power')}>PowerBi</button>
+          <button onClick={()=> hadleCategoryChange('nodejs')}>NodeJs</button>
+
+        </div>
         <div className="projects">
           <div class="container text-center">
-            <div class="row">
-              <div class="col-sm-1 col-md-4 col-lg-4"><img className="compu" src={Cima}></img></div>
+            <div class="row mostrar">
+
+              {filteredProjects.map((project, index)=> (
+                <div key={index} className='col-sm-1 col-md-4 col-lg-4'>
+                  <img className='compu' src={project.src} alt= {`Project ${index}`}/>
+                </div>
+              ))}
+
+              {/*<div class="col-sm-1 col-md-4 col-lg-4"><img className="compu" src={Cima}></img></div>
               <div class="col-sm-1 col-md-4 col-lg-4"><img className="compu" src={Diario}></img></div>
               <div class="col-sm-1 col-md-4 col-lg-4"><img className="compu" src={Disc}></img></div>
               <div class="col-sm-1 col-md-4 col-lg-4"><img className="compu" src={Power}></img></div>
               <div class="col-sm-1 col-md-4 col-lg-4"><img className="compu" src={Star}></img></div>
-              <div class="col-sm-1 col-md-4 col-lg-4"><img className="compu" src={Md}></img></div>
+              <div class="col-sm-1 col-md-4 col-lg-4"><img className="compu" src={Md}></img></div> */}
+
             </div>
           </div>
         </div>
